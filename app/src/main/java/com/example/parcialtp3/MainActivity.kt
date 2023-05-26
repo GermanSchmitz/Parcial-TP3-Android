2a69fe875e5f00e93c9da274eb8140a42d643c76
German Schmitz <germanschmitz123@gmail.com>
2023-05-26 17:36:57 -0300

    navegacion entre login a la activity navegacion

--- app/src/main/java/com/example/parcialtp3/MainActivity.kt
+++ app/src/main/java/com/example/parcialtp3/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.parcialtp3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttonLogin)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, BottomNavigationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
