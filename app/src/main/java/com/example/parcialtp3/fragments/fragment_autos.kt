94cbc5b4e04a74c6bd7efb7b1620119829b9dfee
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2023-05-30 11:26:38 -0300

    bajé los cambios de master y resolví los conflictos

--- app/src/main/java/com/example/parcialtp3/fragments/fragment_autos.kt
+++ app/src/main/java/com/example/parcialtp3/fragments/fragment_autos.kt
@@ -19,19 +19,12 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [fragment_auto.newInstance] factory method to
+ * Use the [fragment_autos.newInstance] factory method to
  * create an instance of this fragment.
  */
-<<<<<<< HEAD:app/src/main/java/com/example/parcialtp3/fragments/fragment_auto.kt
-class fragment_auto : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-=======
 class fragment_autos : Fragment() {
     private lateinit var carViewModel: CarViewModel
     private lateinit var carAdapter: CarAdapter
->>>>>>> origin/master:app/src/main/java/com/example/parcialtp3/fragments/fragment_autos.kt
 
     // Método llamado al crear la vista del fragmento
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -71,35 +64,4 @@ class fragment_autos : Fragment() {
             Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
         }
     }
-<<<<<<< HEAD:app/src/main/java/com/example/parcialtp3/fragments/fragment_auto.kt
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prueba2, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment prueba2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_auto().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-=======
->>>>>>> origin/master:app/src/main/java/com/example/parcialtp3/fragments/fragment_autos.kt
 }
