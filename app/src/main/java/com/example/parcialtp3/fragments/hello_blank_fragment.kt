718c686602ddf608de855cac6599bc291b4b172c
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2023-05-30 00:37:10 -0300

    cambios en el fragment home y auto

--- app/src/main/java/com/example/parcialtp3/fragments/hello_blank_fragment.kt
+++ app/src/main/java/com/example/parcialtp3/fragments/hello_blank_fragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.parcialtp3.R
-import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,18 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [fragment_home.newInstance] factory method to
+ * Use the [hello_blank_fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class fragment_home : Fragment() {
+class hello_blank_fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    lateinit var vista : View
-    lateinit var text1: TextView
-    lateinit var text2: TextView
-    lateinit var text3: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,27 +34,8 @@ class fragment_home : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        vista = inflater.inflate(R.layout.fragment_prueba1, container, false)
-        text1 = vista.findViewById(R.id.text1)
-        text2 = vista.findViewById(R.id.text2)
-        text3 = vista.findViewById(R.id.text3)
-
-        text1.setOnClickListener {
-            //val action = fragment_home.Directions.actionItemHomeToItemAutos()
-            findNavController().navigate(R.id.item_autos)
-        }
-
-        text2.setOnClickListener {
-            //val action = fragment_homeDirections.action_item_home_to_item_autos()
-            findNavController().navigate(R.id.item_autos)
-        }
-
-        text3.setOnClickListener {
-            //val action = fragment_homeDirections.action_item_home_to_item_autos()
-            findNavController().navigate(R.id.item_autos)
-        }
-        return vista
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hello_blank_fragment, container, false)
     }
 
     companion object {
@@ -71,12 +45,12 @@ class fragment_home : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment prueba1.
+         * @return A new instance of fragment hello_blank_fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            fragment_home().apply {
+            hello_blank_fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
